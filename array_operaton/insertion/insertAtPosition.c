9185c549b7a9873bd9642bf3d5b0fae4d3d6f740
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-04-08 07:37:56 +0530

    insertAt position is completed

---
+++ array_operaton/insertion/insertAtPosition.c
@@ -0,0 +1,88 @@
+// insertAtPosition
+
+// preprocessor 
+#include<stdio.h>
+#include<conio.h>
+
+// defined the  array size 
+#define size 5
+
+// declare  the global variables 
+int idx ; 
+
+// create  the array with the size of size 
+// global array access to the every fucntion 
+
+int arr[size]; 
+
+// function declration 
+int isEmpty(); 
+int isFull(); 
+void insertAtPosition(int data , int position);
+
+// main methods start here ⭐⭐⭐⭐
+int main(){
+
+    // set the value of the idx = -1 
+    idx = -1 ; 
+
+    // ⭐ calling the function ⭐
+    insertAtPosition(4,4);
+    return 0; 
+}
+
+
+
+
+// function initalisation 
+
+// isEmpty
+int isEmpty(){
+    if (idx ==-1)
+    {
+        return 1; 
+    }else {
+        return 0;
+    }
+    
+}
+
+
+
+// isFull
+
+int isFull(){
+    if (idx == size-1)
+    {
+        return 1; 
+    }
+    else
+    {
+        return 0; 
+    }
+}
+
+// insertedAtPosition
+
+void insertAtPosition(int data , int position){
+    // check for the full case 
+    if (isFull()==1)
+    {
+        // print the full case message 
+        printf("array is full \n");
+    }
+
+    for (int  i =  idx; i >position-1; i--)
+    {
+        arr[i+1] = arr[i];
+    }
+    
+
+    // ⭐⭐⭐⭐  here we take the postion not index of the array 
+    //as we know that array index is start from the 0 so we have to 
+    // subtaract by 1 in  postion 
+
+    arr[position-1]= data; 
+    printf(" %d data is inserted at postion %d \n ",data , position );
+
+}
