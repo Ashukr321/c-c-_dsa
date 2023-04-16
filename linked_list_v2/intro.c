74ba36868c452b4ef59d23a5c4e02016df2f4b21
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-04-17 00:38:40 +0530

    linked list version 2 intro

---
+++ linked_list_v2/intro.c
@@ -0,0 +1,29 @@
+// why linked list  :) 
+
+// int  *ptr = new int [5]= > this is created inside the heap memory 
+// pointer ptr is created inside the stack 
+
+// we want variables size data structure 
+// so we need to used the linked list data structure 
+// we must  have to create the memory  in heap during the runtime of the programme 
+
+// we always create the linked list in the heap  memory 
+
+// for defining the node we need two things 
+// data , pointer 
+// data can be any type of data
+// pointer is pointing to the node of the data 
+
+// pointer is having of its own type 
+
+// two pointer can pointing the same node 
+
+
+
+// how a node is created 
+// for creating a node first we need a pointer  bec node is created in the heap 
+
+// struct node *p 
+// malloc function return void type of pointer so we have to typecast 
+
+// structure member is access by the -> arrow 
