1a8a7787b27d1bedf6958255b4b7cda35acb54a7
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-04-18 21:12:15 +0530

    node cratation completed

--- linked_list_clg/create_node.c
+++ linked_list_clg/create_node.c
@@ -15,13 +15,21 @@ struct Node *createNode(int data);
 
 int main()
 {
-    struct Node* n1; 
-    n1= createNode(10);
-    n1= createNode(20);
-    n1= createNode(30);
-    n1= createNode(40);
-    return 0;
+    struct Node *n1,*n2,*n3;
+    n1 = createNode(10);
+    n2 = createNode(20);
+    n3 = createNode(30);
+
+
+    // how to link the node of the linked list 
+    n1-> next = n2; 
+    n2->next= n3;
+    printf("%d \n",n1->data);
+    printf("%d\n",n1->next->data);
+    printf("%d\n",n1->next->next->data);
 }
+
+
 
 // initiaisations  the function
 
@@ -35,5 +43,7 @@ struct Node *createNode(int data)
 
     n->data = data;
     n->next = NULL;
+
     printf("node is created of %d \n", n->data);
+    return n;
 }
