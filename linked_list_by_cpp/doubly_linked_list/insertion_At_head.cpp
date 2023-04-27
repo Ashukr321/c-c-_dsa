ca181eed61acf4f08202c24d4cd872529689c4ae
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-04-27 22:19:13 +0530

    insertion in head in doubly linked list is completed

---
+++ linked_list_by_cpp/doubly_linked_list/insertion_At_head.cpp
@@ -0,0 +1,59 @@
+// here we see the how to  insert node in the head of  the  linked list
+// preprocessor
+#include <iostream>
+using namespace std;
+
+// craete the class of the node
+class Node
+{
+    // access modifiers
+public:
+    int data;
+    Node *next;
+    Node *prev;
+
+    // here we create the  constructor  to set the value of the data
+    // constructor name must be same as the class name
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+// print the data of the dll
+void print(Node *&head)
+{
+    // create the new Node temp
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+}
+
+// insertion of the node at the head of the linked list
+void insertAtHead(Node * &head, int data)
+{
+    // here we  first have to create the a new node data for the new data
+    Node *temp = new Node(data);
+    // here we connect the node of the new node to the head node
+
+    temp->next = head;
+    head->prev = temp;
+
+    head = temp; // here we upate the head pointer of the node
+}
+
+// here start the main method
+int main()
+{
+    // create the node
+    Node *node1 = new Node(34);
+    Node *head = node1;
+    insertAtHead(head,24);
+    print(head);
+    return 0;
+}
