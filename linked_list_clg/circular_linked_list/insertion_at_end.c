b3ead51d24ea32fb21cf7444928736c5ec19ebdb
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-05-03 12:40:16 +0530

    cicular sinlgy list  insertion at end of the node

--- linked_list_clg/circular_linked_list/insertion_at_end.c
+++ linked_list_clg/circular_linked_list/insertion_at_end.c
@@ -35,35 +35,61 @@ struct Node *insertAtBegin(struct Node *head, int data)
         return head;
     }
 
-    // create a newNode of the data that commin in the function 
+    // create a newNode of the data that commin in the function
     struct Node *temp = head;
 
-    struct Node *newNode ;
+    struct Node *newNode;
     newNode = createSinglyCircularList(data);
-    while (temp->next!= head)
+    while (temp->next != head)
     {
         temp = temp->next;
     }
-    newNode->next= head;
+    newNode->next = head;
     head = newNode;
-    temp->next= newNode;
+    temp->next = newNode;
+    return head;
+}
+
+// insert at end of the circular linked list
+struct Node *insertAtEnd(struct Node *head,int data)
+{
+
+    // check for the empty case
+    if (head == NULL)
+    {
+        head = createSinglyCircularList(data);
+        return head;
+    }
+
+    // create a temp node
+    struct Node *temp = head;
+    // create a newNode
+    struct Node *newNode = createSinglyCircularList(data);
+
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+
+    // here we apply our logic
+    temp->next = newNode;
+    newNode->next = head;
     return head;
 
-    
 }
 
-
-
-//traverse the circular sinlgy  linked list 
-void traverseCircularSll(struct Node *head){
-   struct Node *temp  = head ;
-   while (temp->next!= head)
-   {
-        printf("%d ->",temp->data);
+// traverse the circular sinlgy  linked list
+void traverseCircularSll(struct Node *head)
+{
+    struct Node *temp = head;
+    while (temp->next != head)
+    {
+        printf("%d ->", temp->data);
         temp = temp->next;
-   }
-   
+    }
 }
+
+
 // main methods start
 int main()
 {
@@ -71,11 +97,15 @@ int main()
 
     n1 = createSinglyCircularList(34);
     head = n1;
+
     head = insertAtBegin(head, 40);
     head = insertAtBegin(head, 10);
     head = insertAtBegin(head, 50);
     head = insertAtBegin(head, 20);
-    head = insertAtBegin(head,2000);
+    head = insertAtBegin(head, 2000);
+    head = insertAtBegin(head, 20);
+    head = insertAtBegin(head, 10);
+    head= insertAtEnd(head,300);
 
     traverseCircularSll(head);
     return 0;
