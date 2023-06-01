0f8509b62ab90b1a6d9ca73c30965fa4c1a45694
Ashukr321 <ashutoshkumarsingh55555@gmail.com>
2023-06-01 15:48:55 +0530

    fibonacci number by recursion is completed

---
+++ recursion/n_fibanacci_number.cpp
@@ -0,0 +1,21 @@
+// fibonacci number 
+#include<iostream>
+using namespace std;
+
+int fibonacci(int n){
+    
+    if (n==0||n==1)
+    {
+        return n;
+    }
+    
+    return fibonacci(n-1)+fibonacci(n-2);
+  
+}
+int main(){
+    int n = 5; 
+    int value = fibonacci(n);
+    cout<<value<<endl;
+
+    return 0; 
+}
